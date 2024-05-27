f61066675df02bb5cf5cc40f781d049654721a5d
hexs <zxjq97@gmail.com>
2024-05-27 14:30:56 +0700

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 ﻿from setuptools import setup, find_packages
 
 setup(
-    name='Ruin',
+    name='ruin',
     version='2024.0.1',
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "ruin-hello = runt_hello:hello"
+        ],
+    },
 )
